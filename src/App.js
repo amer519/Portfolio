0564291917f6bb6a8d337fe1843d58650c0fb5c9
amer519 <afahmy519@gmail.com>
2020-09-20 15:37:46 +0200

    set-up

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'Amer Fahmy',
+      headerLinks: [
+        { title: 'Home', path: '/' },
+        { title: 'About', path: '/about' },
+        { title: 'Contact', path: '/contact' }
+      ],
+      home: {
+        title: 'big font title',
+        subTitile: 'small font title',
+        text: 'dummy text'
+      },
+      about: {
+        title: 'About Me',
+        
+      },
+      contact: {
+        title: 'Hey, feel free to contact me anytime',
+        
+      }
+    }
+  }
+
+  render() {
+   return (
+    <div>
+      Wasssupppppp!!!
     </div>
-  );
+  ); 
+  }
+  
 }
 
 export default App;
